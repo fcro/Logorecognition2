2f6c6efc25ed748a2eaa3aa64cb9d1de7549dbc7
fcro <floriancrosnier@hacari.net>
2017-01-22 22:54:52 +0100

    Classe FileManager pour gérer les fichiers de BoW + Récupération vocabulary.yml + Appel de setVocabulary

--- app/src/main/java/com/telecom/cottoncrosnier/logorecognition2/Classifier.java
+++ app/src/main/java/com/telecom/cottoncrosnier/logorecognition2/Classifier.java
@@ -2,7 +2,6 @@ package com.telecom.cottoncrosnier.logorecognition2;
 
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -21,9 +20,6 @@ import org.bytedeco.javacpp.opencv_ml.CvSVM;
 import org.bytedeco.javacpp.opencv_nonfree.SIFT;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by matthieu on 19/01/17.
@@ -39,10 +35,10 @@ public class Classifier {
     private BOWImgDescriptorExtractor mBowide;
     private SIFT detector;
 
-    public Classifier(Context contextMain) { // TODO rajouter throw filenullexeption
+    public Classifier(Context mainContext) { // TODO rajouter throw filenullexeption
 
         detector = new SIFT(0, 3, 0.04, 10, 1.6);
-        mMainContext = contextMain;
+        mMainContext = mainContext;
         mClassName[0] = "Coca";
         mClassName[1] = "Pepsi";
         mClassName[2] = "Sprite";
@@ -54,7 +50,6 @@ public class Classifier {
         final FlannBasedMatcher matcher = new FlannBasedMatcher();
 
         mBowide = new BOWImgDescriptorExtractor(detector.asDescriptorExtractor(), matcher);
-//        createVocabulary();
 
 //        File image = Utils.assetToCache(mMainContext, ref+imageName+".jpg", imageName+".jpg");
 //        Mat response_hist = computeHist(image.getPath(), detector, mBowide);
@@ -69,14 +64,17 @@ public class Classifier {
 
     }
 
-    private void createVocabulary() {
-        Log.d(TAG, "createVocabulary() called");
+
+    public void setVocabulary(File vocabularyFile) {
+        Log.d(TAG, "setVocabulary() called with: vocabularyFile = [" + vocabularyFile.getAbsolutePath() + "]");
         final Mat vocabulary;
         Loader.load(opencv_core.class);
+        Log.d(TAG, "setVocabulary: opencv core");
 
-        File fileVoca = Utils.assetToCache(mMainContext, "vocabulary.yml", "vocabulary.yml");
-
-        opencv_core.CvFileStorage storage = opencv_core.cvOpenFileStorage(fileVoca.getAbsolutePath(), null, opencv_core.CV_STORAGE_READ);
+        opencv_core.CvFileStorage storage = opencv_core.cvOpenFileStorage(
+//                Utils.assetToCache(mMainContext, "vocabulary.yml", "vocabulary.yml").getAbsolutePath(),
+                vocabularyFile.getAbsolutePath(),
+                null, opencv_core.CV_STORAGE_READ);
         Pointer p = opencv_core.cvReadByName(storage, null, "vocabulary", opencv_core.cvAttrList());
         opencv_core.CvMat cvMat = new opencv_core.CvMat(p);
         vocabulary = new Mat(cvMat);
@@ -85,16 +83,15 @@ public class Classifier {
         mBowide.setVocabulary(vocabulary);
     }
 
-    private Mat computeHist(String imgPath, SIFT detector, BOWImgDescriptorExtractor bowide) {
-
-        Log.d(TAG, "computeHist() called with: imgPath = [" + imgPath + "], detector = [" + detector + "], bowide = [" + bowide + "]");
+    private Mat computeHist(String imgPath, SIFT detector) {
+        Log.d(TAG, "computeHist() called with: imgPath = [" + imgPath + "], detector = [" + detector + "]");
         Mat response_hist = new Mat();
         KeyPoint keypoints = new KeyPoint();
         Mat inputDescriptors = new Mat();
 
         Mat matImage = imread(imgPath, 1);
         detector.detectAndCompute(matImage, Mat.EMPTY, keypoints, inputDescriptors);
-        bowide.compute(matImage, keypoints, response_hist);
+        mBowide.compute(matImage, keypoints, response_hist);
 
         return response_hist;
     }
@@ -128,14 +125,13 @@ public class Classifier {
     }
 
 
-    public void computeImageHist(Uri path){
+    public void computeImageHist(Uri path) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = Utils.scaleBitmapDown(BitmapFactory.decodeFile(path.getPath(), options),500);
+        Bitmap bitmap = Utils.scaleBitmapDown(BitmapFactory.decodeFile(path.getPath(), options), 500);
 
         File bitmapFile = Utils.bitmapToCache(mMainContext, bitmap, path.getLastPathSegment());
 
-        Mat response_hist = computeHist(bitmapFile.getPath(), detector, mBowide);
+        Mat response_hist = computeHist(bitmapFile.getAbsolutePath(), detector);
     }
-
 }
---
+++ app/src/main/java/com/telecom/cottoncrosnier/logorecognition2/FileManager.java
@@ -0,0 +1,60 @@
+package com.telecom.cottoncrosnier.logorecognition2;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by User on 21/01/2017.
+ */
+
+public class FileManager {
+
+    private static final String TAG = FileManager.class.getSimpleName();
+
+
+    /**
+     * Compare le fichier de vocabulaire existant avec {@code content}. Si ils ont un poids
+     * différent, remplace le fichier de vocabulaire par {@code content}. Retourne toujours
+     * le fichier de vocabulaire.
+     *
+     * @param cacheDir répertoire cache de l'application pour stocker le fichier vocabulary.yml.
+     * @param content contenu du vocabulary.yml reçu.
+     * @return l'objet {@link File} de vocabulary.yml dans le cache.
+     * @throws IOException si une erreur se produit pendant la lecture ou l'écriture du fichier
+     * vocabulary.yml.
+     */
+    public static File createVocabularyFile(File cacheDir, String content) throws IOException {
+        Log.d(TAG, "createVocabularyFile() called with: cacheDir = [" + cacheDir + "], content = [" + "..." + "]");
+        File vocabularyFile = new File(cacheDir.getPath() + "/vocabulary.yml");
+        long currentFileSize = vocabularyFile.length();
+        FileWriter fw = new FileWriter(vocabularyFile, false);
+
+        try {
+            if (vocabularyFile.createNewFile()) {
+                Log.d(TAG, "createVocabularyFile: vocabulary file does not exist");
+                fw.write(content);
+            } else {
+                Log.d(TAG, "createVocabularyFile: filesize = " + currentFileSize);
+                long newFileSize = content.length();
+                Log.d(TAG, "createVocabularyFile: newfilesize = " + newFileSize);
+                if (newFileSize != currentFileSize) {
+                    fw.write(content);
+                }
+            }
+
+            return vocabularyFile;
+        } finally {
+            try {
+                fw.flush();
+                Log.d(TAG, "createVocabularyFile: flushed");
+                fw.close();
+                Log.d(TAG, "createVocabularyFile: closed");
+            } catch (IOException e) {
+                Log.e(TAG, "createVocabularyFile: failed flushing/closing filewriter");
+            }
+        }
+    }
+}
